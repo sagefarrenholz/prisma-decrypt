4330b8f822e92e0a1371ac8f1bc3ceb59fcbfecc
sagefarrenholz <46610156+sagefarrenholz@users.noreply.github.com>
2025-06-01 11:45:18 -0500

    sha

--- Formula/prisma-decrypt.rb
+++ Formula/prisma-decrypt.rb
@@ -2,13 +2,13 @@ class PrismaDecrypt < Formula
   desc "CLI tool for super fast prisma-field-encryption column decryption (clock decryption)"
   homepage "https://github.com/sagefarrenholz/prisma-decrypt"
   url "https://github.com/sagefarrenholz/prisma-decrypt/releases/download/v0.1.2/prisma-decrypt-macos-intel.tar.gz"
-  sha256 "UPDATE_WITH_ACTUAL_SHA256_HASH"
+  sha256 "e2dbb53cbf48b768e1a7000f47e3fc4bf07807b60eb432566e2dcce9dd1dcfc1"
   version "0.1.2"
   license "MIT OR Apache-2.0"
 
   on_arm do
     url "https://github.com/sagefarrenholz/prisma-decrypt/releases/download/v0.1.2/prisma-decrypt-macos-arm64.tar.gz"
-    sha256 "UPDATE_WITH_ACTUAL_ARM64_SHA256_HASH"
+    sha256 "366b047f666ad6713137c33dd9713a379d7788151b588a7c6ada3deb318749a6"
   end
 
   def install
