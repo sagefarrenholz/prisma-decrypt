e6be20389986ecbdc246f28f6e503d7aa64d2759
sagefarrenholz <46610156+sagefarrenholz@users.noreply.github.com>
2025-06-01 11:36:54 -0500

    bump

--- Formula/prisma-decrypt.rb
+++ Formula/prisma-decrypt.rb
@@ -3,7 +3,7 @@ class PrismaDecrypt < Formula
   homepage "https://github.com/sagefarrenholz/prisma-decrypt"
   url "https://github.com/sagefarrenholz/prisma-decrypt/releases/download/v0.1.0/prisma-decrypt-macos-intel.tar.gz"
   sha256 "UPDATE_WITH_ACTUAL_SHA256_HASH"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT OR Apache-2.0"
 
   on_arm do
